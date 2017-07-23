f199ac1653ace57589b19bbf6520de8a00465a4b
DESKTOP-308JDVG\chichikolon <djimgoupat@yahoo.fr>
2017-07-23 15:00:32 +0200

    up

---
+++ HaarCascade/PlayingCard/main.py
@@ -0,0 +1,15 @@
+from webcam import Webcam
+ 
+webcam = Webcam()
+
+
+#webcam.detect_cards_local()
+  
+# play a game of cards
+while True:
+  
+    # attempt to detect the three of hearts
+    if webcam.detect_cards('haarcascade_3hearts.xml') == True:
+        print "I have the cotton picking three of hearts"
+    else:
+        print "I do not have the darn gun slinging three of hearts"
