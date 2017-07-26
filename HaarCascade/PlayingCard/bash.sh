fced56be85387c77ed091a99253bc2c40b2bf1e7
chichikolon <djimgoupat@yahoo.fr>
2017-07-26 17:27:20 +0200

    up

---
+++ HaarCascade/PlayingCard/bash.sh
@@ -0,0 +1,6 @@
+opencv_createsamples -img 3hearts.png -bg bg.txt -info info/info.lst -pngoutput info -maxxangle 1.1 -maxyangle 1.1 -maxzangle 0.5 -num 1950
+
+opencv_createsamples -info info/info.lst -num 1950 -w 50 -h 75 -vec positives.vec
+
+opencv_traincascade -data data -vec positives.vec -bg bg.txt -numPos 400 -numNeg 200 -numStages 4 -w 50 -h 75 &
+
